da83ff19927fd0a8f2d850f555fa9a9f6032d755
Sanam <sanampakuwal27@gmail.com>
2023-02-07 16:40:13 +0545

    chore: connection string from json

---
+++ Dtos/ConnectionStringDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Dtos;
+
+internal class ConnectionStringDto
+{
+    public string SqlServerConnectionString { get; set; }
+    public string PostgreSqlConnectionString { get; set; }
+}
